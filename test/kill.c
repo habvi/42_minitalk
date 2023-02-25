84fddcd55966ad1f8ca93f7154a2d27368d621e3
habvi <a2231250601h@gmail.com>
2023-02-26 05:52:07 +0900

    test: send string

--- test/kill.c
+++ test/kill.c
@@ -3,6 +3,9 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdint.h>
 
 #define ERROR -1
 
@@ -10,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	pid_t	pid;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
 		printf("usage : %s pid\n", argv[0]);
 		exit(EXIT_FAILURE);
@@ -23,22 +26,58 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	// success: 0, error: -1
-	int res = kill(pid, SIGUSR1);
-	if (res == ERROR)
+	const char	*message = argv[2];
+	printf("received message: %s\n", message);
+	uint8_t	i;
+	uint8_t	j;
+	char	c;
+	i = 0;
+	while (message[i])
 	{
-		if (errno == EPERM)
-			printf("process exists, but don't have permission to send it a signal\n");
-		else if (errno == ESRCH)
-			printf("process doesn't exist\n");
-		else
+		c = message[i];
+		j = 0;
+		while (j < CHAR_BIT)
 		{
-			perror("kill");
-			exit(EXIT_FAILURE);
+			if (((c >> (CHAR_BIT - j - 1)) & 1) == 0)
+			{
+				if (kill(pid, SIGUSR1) == ERROR)
+					exit(EXIT_FAILURE);
+			}
+			else
+			{
+				if (kill(pid, SIGUSR2) == ERROR)
+					exit(EXIT_FAILURE);
+			}
+			usleep(500);
+			j++;
 		}
+		i++;
 	}
-	else
-		printf("process exists, success to send it a signal\n");
+	i = 0;
+	while (i < CHAR_BIT)
+	{
+		if (kill(pid, SIGUSR1) == ERROR)
+			exit(EXIT_FAILURE);
+		usleep(500);
+		i++;
+	}
+
+	// success: 0, error: -1
+	// int res = kill(pid, SIGUSR1);
+	// if (res == ERROR)
+	// {
+	// 	if (errno == EPERM)
+	// 		printf("process exists, but don't have permission to send it a signal\n");
+	// 	else if (errno == ESRCH)
+	// 		printf("process doesn't exist\n");
+	// 	else
+	// 	{
+	// 		perror("kill");
+	// 		exit(EXIT_FAILURE);
+	// 	}
+	// }
+	// else
+	// 	printf("process exists, success to send it a signal\n");
 	exit(EXIT_SUCCESS);
 }
 
--- test/sigaction.c
+++ test/sigaction.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <signal.h>
 #include <string.h>
+#include <limits.h>
+#include <stdint.h>
 
 #define ERROR -1
 
@@ -17,23 +19,47 @@
 // 	void		(*sa_restorer)(void);
 // };
 
-volatile sig_atomic_t	g_finish = 0;
+volatile sig_atomic_t	g_char = 0;
 
 void	signal_handler(int signum)
 {
+	static uint8_t	len = 0;
+	char			c;
+
 	if (signum == SIGUSR1)
 	{
-		write(1, "1 dayo\n", 8);
-		g_finish = 1;
+		// if (write(STDOUT_FILENO, "0 dayo\n", 8) == ERROR)
+		// 	exit(EXIT_FAILURE);
+		g_char <<= 1;
 	}
 	else if (signum == SIGUSR2)
-		write(1, "2 dayo\n", 8);
+	{
+		// if (write(STDOUT_FILENO, "1 dayo\n", 8) == ERROR)
+		// 	exit(EXIT_FAILURE);
+		g_char = (g_char << 1) | 1;
+	}
+	len++;
+	if (len == CHAR_BIT)
+	{
+		c = g_char & ((1 << CHAR_BIT) - 1);
+		if (c == '\0')
+		{
+			// if (write(STDOUT_FILENO, "end\n", 5) == ERROR)
+			// 	exit(EXIT_FAILURE);
+			g_char = 0;
+			len = 0;
+			return ;
+		}
+		if (write(STDOUT_FILENO, &c, 1) == ERROR)
+			exit(EXIT_FAILURE);
+		g_char = 0;
+		len = 0;
+	}
 }
 
 int	main(void)
 {
 	struct sigaction	sa;
-	size_t				i;
 
 	printf("pid %d\n", getpid());
 	memset(&sa, 0, sizeof(sa)); // いる？
@@ -46,13 +72,9 @@ int	main(void)
 		exit(EXIT_FAILURE);
 	if (sigaction(SIGUSR2, &sa, NULL) == ERROR)
 		exit(EXIT_FAILURE);
-	i = 0;
-	while (!g_finish)
-	{
-		printf("%zu\n", i);
-		sleep(1);
-		i++;
-	}
+
+	while (true)
+		pause();
 	printf("finish!!");
 	return (0);
 }
