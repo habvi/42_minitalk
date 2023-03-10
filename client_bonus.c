cf7c3ae228bdd33e9bc319387fc130c0de066a78
habvi <a2231250601h@gmail.com>
2023-03-10 11:51:47 +0100

    add: bonus base

---
+++ client_bonus.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "minitalk.h"
+
+static bool	is_valid_args(const int argc)
+{
+	if (argc != 3)
+	{
+		// to do
+		printf("usage : tell me PID!!\n");
+		return (false);
+	}
+	return (true);
+}
+
+static bool	is_valid_pid(char *argv[], pid_t *pid)
+{
+	*pid = atoi(argv[1]);
+	// to do: atoi error or pid <= 0
+	if (*pid <= 0)
+	{
+		printf("invalid pid : %d\n", *pid);
+		return (false);
+	}
+	printf("client pid %d\n", *pid);
+	return (true);
+}
+
+static bool	send_message(const pid_t pid, const char *message)
+{
+	uint8_t	i;
+	uint8_t	j;
+	int		c;
+	int		result;
+
+	i = 0;
+	while (message[i])
+	{
+		c = message[i];
+		j = 0;
+		while (j < CHAR_BIT)
+		{
+			if (((c >> (CHAR_BIT - j - 1)) & 1) == 0)
+				result = kill(pid, SIGUSR1);
+			else
+				result = kill(pid, SIGUSR2);
+			if (result == ERROR)
+				return (false);
+			usleep(500);
+			j++;
+		}
+		i++;
+	}
+	// to do: '\0'?
+	i = 0;
+	while (i < CHAR_BIT)
+	{
+		if (kill(pid, SIGUSR1) == ERROR)
+			return (false);
+		usleep(500);
+		i++;
+	}
+	return (true);
+}
+
+static void signal_handler(int signum)
+{
+	static int count = 0;
+
+	if (signum == SIGUSR1)
+	{
+		count++;
+		if (write(STDOUT_FILENO, "recieved message from server!", 30) == ERROR)
+			exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+}
+
+int	main(int argc, char *argv[])
+{
+	pid_t	pid;
+
+	if (!is_valid_args(argc))
+		return (EXIT_FAILURE);
+	if (!is_valid_pid(argv, &pid))
+		return (EXIT_FAILURE);
+	if (signal(SIGUSR1, signal_handler) == SIG_ERR)
+		exit(EXIT_FAILURE);
+	printf("send message from client: %s\n", argv[2]);
+	if (!send_message(pid, argv[2]))
+		return (EXIT_FAILURE);
+	pause();
+	exit(EXIT_SUCCESS);
+}
+
+// to do
+// printf, atoi, memset -> ft
---
+++ server_bonus.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <string.h>
+#include <limits.h>
+#include <stdint.h>
+#include "minitalk.h"
+
+// int sigaction(int sig, const struct sigaction *__restrict__ new, struct sigaction *__restrict__ old);
+
+// struct sigaction {
+// 	void		(*sa_handler)(int);
+// 	void		(*sa_sigaction)(int, siginfo_t *, void *);
+// 	sigset_t	sa_mask;
+// 	int			sa_flags;
+// 	void		(*sa_restorer)(void);
+// };
+
+volatile sig_atomic_t	g_signum = 0;
+// to do: how~
+pid_t client_pid = 0;
+
+void	signal_handler(int signum, siginfo_t *info, void *context)
+{
+	(void)context;
+	g_signum = signum;
+	client_pid = info->si_pid;
+	// if (len == CHAR_BIT)
+	// 	kill(info->si_pid, SIGUSR1);
+}
+
+static bool	put_message(void)
+{
+	static uint8_t	len = 0;
+	static int		c = 0;
+
+	if (g_signum == SIGUSR1)
+		c <<= 1;
+	else if (g_signum == SIGUSR2)
+		c = (c << 1) | 1;
+	len++;
+	if (len == CHAR_BIT)
+	{
+		if (c == '\0')
+		{
+			if (kill(client_pid, SIGUSR1) == ERROR)
+				return (false);
+		}
+		else if (write(STDOUT_FILENO, &c, 1) == ERROR)
+			return (false);
+		g_signum = 0;
+		len = 0;
+		c = 0;
+	}
+	return (true);
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	printf("pid %d\n", getpid());
+	memset(&sa, 0, sizeof(sa));
+	if (sigemptyset(&sa.sa_mask) == ERROR)
+		return (EXIT_FAILURE);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	if (sigaction(SIGUSR1, &sa, NULL) == ERROR)
+		return (EXIT_FAILURE);
+	if (sigaction(SIGUSR2, &sa, NULL) == ERROR)
+		return (EXIT_FAILURE);
+	while (true)
+	{
+		pause();
+		if (!put_message())
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
