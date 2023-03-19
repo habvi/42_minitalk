38d282b91da91636f6e2be25f19edb0763fd18e1
habvi <a2231250601h@gmail.com>
2023-03-19 20:30:18 +0100

    rm: unused libft

--- libft/src/libft/ft_isprint.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-bool	ft_isprint(int c)
-{
-	return (' ' <= c && c <= '~');
-}
--- libft/src/libft/ft_tolower.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-int	ft_tolower(int c)
-{
-	if ('A' <= c && c <= 'Z')
-		c += 'a' - 'A';
-	return (c);
-}
